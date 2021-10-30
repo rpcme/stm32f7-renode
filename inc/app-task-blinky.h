9a8cbfc17c0e56feb312327c8500c2949c54384b
rpcme <rich@richelberger.com>
2021-10-30 10:50:22 -0400

    rename blinky files

---
+++ inc/app-task-blinky.h
@@ -0,0 +1,24 @@
+#ifndef APP_TASK_BLINKY_H
+#define APP_TASK_BLINKY_H
+/* Priorities at which the tasks are created. */
+#define mainQUEUE_RECEIVE_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
+#define	mainQUEUE_SEND_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
+
+/* The rate at which data is sent to the queue.  The 200ms value is converted
+to ticks using the portTICK_PERIOD_MS constant. */
+#define mainQUEUE_SEND_FREQUENCY_MS			( 200 / portTICK_PERIOD_MS )
+
+/* The number of items the queue can hold.  This is 1 as the receive task
+will remove items as they are added, meaning the send task should always find
+the queue empty. */
+#define mainQUEUE_LENGTH					( 1 )
+
+
+void vInitializeTaskBlinky();
+
+
+/*
+ * The tasks as described in the comments at the top of this file.
+ */
+
+#endif
