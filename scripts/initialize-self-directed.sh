94356c0714745596ee6103e94809d91b4f75d009
rpcme <rich@richelberger.com>
2021-11-25 12:29:37 -0500

    remove quiet to see why it is failing

--- scripts/initialize-self-directed.sh
+++ scripts/initialize-self-directed.sh
@@ -1,21 +1,23 @@
 #! /bin/bash
 
 basedir=${HOME}/environment
+cd ${basedir}
 
 sudo yum remove -y openssl-devel
 sudo yum install -y jq openssl11 openssl11-devel
 
-pip3 install --user -U boto boto3 botocore awscli
 export PATH=${HOME}/.local/bin:$PATH
 echo "export PATH=${HOME}/.local/bin:$PATH" >> ~/.bashrc
+echo "source ${HOME}/environment/credentials/*/exports" >> ~/.bashrc
+pip3 install --user -U boto boto3 botocore awscli
 
 git config --global user.name "Cloudy Builder"
 git config --global user.email "developer@builder.me"
 git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 
-mkdir ${HOME}/environment/cmake
-wget -q https://github.com/Kitware/CMake/releases/download/v3.21.4/cmake-3.21.4-linux-x86_64.sh
+mkdir -p ${HOME}/environment/cmake
+wget https://github.com/Kitware/CMake/releases/download/v3.21.4/cmake-3.21.4-linux-x86_64.sh
 sh ./cmake-3.21.4-linux-x86_64.sh --skip-license --prefix=${HOME}/environment/cmake
 echo "export PATH=${HOME}/environment/cmake/bin:$PATH" >> ~/.bashrc
 rm cmake-3.21.4-linux-x86_64.sh
@@ -37,7 +39,7 @@ git push --set-upstream origin main
 git submodule update --init --recursive
 cd ${basedir}
 
-git clone https://github.com/rpcme/stm32f7-renode myapp-config
+#git clone https://github.com/rpcme/stm32f7-renode myapp-config
 
 mkdir ${basedir}/myapp-core/configuration
 cp ${basedir}/myapp-config/configuration/awscsdk-buildspec-build.yml ${basedir}/myapp-core/configuration
